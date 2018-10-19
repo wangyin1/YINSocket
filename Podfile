56726bd6a3a83934c4b9113f043beef4f7f57bf8
wangyin1 <apple@wangyindePro.local>
2018-10-19 15:48:55 +0800

    版本提交

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+target 'YINSocket' do
+
+use_frameworks!
+
+pod 'CocoaAsyncSocket'
+
+end
---
+++ Pods/Target Support Files/Pods-YINSocket/Pods-YINSocket-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YINSocket : NSObject
+@end
+@implementation PodsDummy_Pods_YINSocket
+@end
